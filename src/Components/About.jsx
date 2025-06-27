c0c6d94ee0b3acf58c6b98690630798e1552f9a3
Ruba24 <rubaqazi2000@gmail.com>
2025-06-27 12:59:27 +0500

    Page creation

--- src/Components/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const About = () => {
-  return (
-    <div>I am at About</div>
-  )
-}
-
-export default About
--- src/Components/Login.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Login = () => {
-  return (
-    <div>I am on Login Page</div>
-  )
-}
-
-export default Login
